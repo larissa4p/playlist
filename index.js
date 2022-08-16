487a88d41889f5fdb662241054651b6d291fa1dc
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-08-16 14:10:02 -0300

    connect db

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const connetToDb = require("./database/db");
 
+const app = express();
 const port = process.env.PORT || 3000;
 
+connetToDb();
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
 
 app.listen(port, () =>
