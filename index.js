f9200fc9a1f5109a6e0f41bea8eef06aa3ef112d
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-08-17 14:52:08 -0300

    style

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded());
 
 connetToDb();
